dfe34b36ae7b76cbbc93df2e8172506508e8bc3e
Jan Hafner <icedt89@gmail.com>
2025-12-09 12:31:12 +0100

    v0.2.1
    Try fixing PWA icons

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -4,6 +4,29 @@ import {
 } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset: minimal2023Preset,
+  preset: {
+    ...minimal2023Preset,
+    maskable: {
+      ...minimal2023Preset.maskable,
+      resizeOptions: {
+        ...minimal2023Preset.maskable?.resizeOptions,
+        background: '#141b22',
+      },
+    },
+    apple: {
+      ...minimal2023Preset.apple,
+      resizeOptions: {
+        ...minimal2023Preset.apple?.resizeOptions,
+        background: '#141b22',
+      },
+    },
+    transparent: {
+      ...minimal2023Preset.transparent,
+      resizeOptions: {
+        ...minimal2023Preset.transparent?.resizeOptions,
+        background: '#141b22',
+      },
+    },
+  },
   images: ['public/logo.svg'],
 })
